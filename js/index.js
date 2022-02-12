f64c264a747967313acd1cbd6adfca7084899590
greybenz <umbelzz@gmail.com>
2022-02-12 16:01:09 -0700

    Update 2/12

---
+++ js/index.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $(".img1 img").click(function(){
+      $(".img1 img").animate({
+       left: '250px',
+       opacity: '0.7',
+       height: '350px',
+       width: '450px'
+     });
+ });
+   });
+
+//  $('.img1') .css ('opacity', 0.4); });
